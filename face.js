f3dd1ad7cb0899ed2dc91bbfcea665c852aa82ea
LouisJefferson607 <100631217+LouisJefferson607@users.noreply.github.com>
2023-06-06 11:16:52 +1200

    Imported Face
    I have imported my face.js code into this. Took a bit of debugging but is now roughly working

--- face.js
+++ face.js
@@ -29,7 +29,7 @@ function segment_average(segment) {
 }
 
 // This where you define your own face object
-function Face() {
+function Face(eye_value, mouth_value, chocolate_value, NS, chocolate_size) {
   // these are state variables for a face
   // (your variables should be different!)
   this.detailColour = [204, 136, 17];
@@ -42,74 +42,195 @@ function Face() {
   this.lipColour = [136, 68, 68]
   this.eyebrowColour = [119, 85, 17]
 
+  this.eye_value = int(random(3, 8));
+  this.mouth_value = random(0, 150);
+  this.chocolate_value = int(random(0, 14));
+  this.height_cook = int(random(canvasHeight / 2 - 140, canvasHeight / 2 + 140));
+  this.width_cook = int(random(canvasWidth /  2 - 250, canvasWidth / 2 + 250));
+  this.chocolate_size = int(random(1, 2));
+
   /*
    * Draw the face with position lists that include:
    *    chin, right_eye, left_eye, right_eyebrow, left_eyebrow
    *    bottom_lip, top_lip, nose_tip, nose_bridge, 
    */  
   this.draw = function(positions) {
-    console.log()
-    // head
-    ellipseMode(CENTER);
-    stroke(stroke_color);
-    fill(this.mainColour);
-    ellipse(segment_average(positions.chin)[0], 0, 3, 4);
-    noStroke();
+    let cookie_colors = [
+      {color: color(132, 86, 60), weight: 5},
+      {color: color(217, 190, 145), weight: 3},
+      {color: color(231, 206, 150), weight: 2},
+      {color: color(238, 224, 177), weight: 1},
+      {color: color(37, 32, 27), weight: 0.1, burnt: true},
+      {color: color(255, 255, 255), weight: 0.01, uncooked: true},
+    ];
 
-
-    // mouth
-    fill(this.detailColour);
-    ellipse(segment_average(positions.bottom_lip)[0], segment_average(positions.bottom_lip)[1], 1.36, 0.25 * this.mouth_size);
-
-    // eyebrows
-    fill( this.eyebrowColour);
-    stroke( this.eyebrowColour);
-    strokeWeight(0.08);
-    this.draw_segment(positions.left_eyebrow);
-    this.draw_segment(positions.right_eyebrow);
-
-    // draw the chin segment using points
-    fill(this.chinColour);
-    stroke(this.chinColour);
-    this.draw_segment(positions.chin);
-
-    fill(100, 0, 100);
-    stroke(100, 0, 100);
-    this.draw_segment(positions.nose_bridge);
-    this.draw_segment(positions.nose_tip);
-
-    strokeWeight(0.03);
-
-    fill(this.lipColour);
-    stroke(this.lipColour);
-    this.draw_segment(positions.top_lip);
-    this.draw_segment(positions.bottom_lip);
-
-    let left_eye_pos = segment_average(positions.left_eye);
-    let right_eye_pos = segment_average(positions.right_eye);
-
-    // eyes
-    noStroke();
-    let curEyeShift = 0.04 * this.eye_shift;
-    if(this.num_eyes == 2) {
-      fill(this.detailColour);
-      ellipse(left_eye_pos[0], left_eye_pos[1], 0.5, 0.33);
-      ellipse(right_eye_pos[0], right_eye_pos[1], 0.5, 0.33);
-
-      // fill(this.mainColour);
-      // ellipse(left_eye_pos[0] + curEyeShift, left_eye_pos[1], 0.18);
-      // ellipse(right_eye_pos[0] + curEyeShift, right_eye_pos[1], 0.18);
+    // Calculate the total weight of all the colors
+    let total_weight = 0;
+    for (let i = 0; i < cookie_colors.length; i++) {
+      total_weight += cookie_colors[i].weight;
     }
-    else {
-      let eyePosX = (left_eye_pos[0] + right_eye_pos[0]) / 2;
-      let eyePosY = (left_eye_pos[1] + right_eye_pos[1]) / 2;
 
-      fill(this.detailColour);
-      ellipse(eyePosX, eyePosY, 0.45, 0.27);
-
-      fill(this.mainColour);
-      ellipse(eyePosX - 0.1 + curEyeShift, eyePosY, 0.18);
+    // Randomly select a color based on its weight
+    let random_weight = random(0, total_weight);
+    let random_cookie_color;
+    for (let i = 0; i < cookie_colors.length; i++) {
+      random_weight -= cookie_colors[i].weight;
+      if (random_weight <= 0) {
+        random_cookie_color = cookie_colors[i].color;
+        if (cookie_colors[i].uncooked) {
+          fill(255); // uncooked color
+        } else if (cookie_colors[i].burnt) {
+          fill(37, 32, 27); // burnt color
+        } else {
+          fill(random_cookie_color); // normal color
+        }
+        break;
+      }
     }
+  //Gets random noise seed so that all cookies are drawn differently
+  //Draws cookies shadow using Perlin noise
+  noiseSeed(NS)
+    angleMode(RADIANS)
+  noStroke()
+    let shadow_offset = 1;
+    let shadow_color = color(0, 0, 0, 200);
+    push();
+    translate(shadow_offset, shadow_offset);
+    fill(shadow_color);
+    beginShape();
+    let noiseMax = 0.5; // set maximum noise value to 0.5
+    for (let angle = 0; angle <= 360; angle += 10) {
+      let xOff = map(cos(radians(angle)), -1, 1, 0, noiseMax);
+      let yOff = map(sin(radians(angle)), -1, 1, 0, noiseMax);
+      let noiseVal = noise(xOff * 2, yOff * 2); // multiply xOff and yOff by 2 for a smaller noise size
+      let radius = 5 + (noiseVal * 5); // set radius range to 5-10 for a smaller cookie size
+      let x = radius * cos(radians(angle));
+      let y = radius * sin(radians(angle));
+      curveVertex(x, y);
+    }
+    endShape(CLOSE);
+    //Draws cookies using Perlin noise
+    pop();
+    beginShape();
+    stroke(1);
+    noiseMax = 0.5; // set maximum noise value to 0.5
+    for (let angle = 0; angle <= 360; angle += 10) {
+      let xOff = map(cos(radians(angle)), -1, 1, 0, noiseMax);
+      let yOff = map(sin(radians(angle)), -1, 1, 0, noiseMax);
+      let noiseVal = noise(xOff*2, yOff*2); // multiply xOff and yOff by 2 for a smaller noise size
+      let radius = 5 + (noiseVal * 5); // set radius range to 5-10 for a smaller cookie size
+      let x = radius * cos(radians(angle));
+      let y = radius * sin(radians(angle));
+      curveVertex(x, y);
+    }
+    endShape(CLOSE);
+
+    
+    // If cookie is brown color , choc chip cookies are white. Otherise they are breown
+    noStroke();
+    ;
+    if (random_cookie_color !== cookie_colors[0].color) {
+      fill(37, 32, 27); // chocolate color
+    } else {
+      fill(237, 230, 214); // white color for chocolate chips if the cookie is the first color in the array
+    }
+
+     //Draws choc chips
+     for (let i = 0; i < this.chocolate_value; i++) {
+      let x = random(-5, 5);
+      let y = random(-5, 5);
+      ellipse(x, y, this.chocolate_size, this.chocolate_size);
+    }
+     // draw eyes
+     stroke(1);
+     fill(255);
+     ellipse(-3, -2, this.eye_value, this.eye_value);
+     ellipse(3, -2, this.eye_value, this.eye_value);
+     
+     // draw black pupils in the eyes
+     fill(0);
+     let pupil_size = this.eye_value * 0.5; // size of the pupil
+     let pupil_offset = this.eye_value * 0.2; // offset of the pupil from the center of the eye
+     ellipse(-3, -2, pupil_size, pupil_size);
+     ellipse(3, -2, pupil_size, pupil_size);
+     
+     // draw two small white circles inside each pupil relative to eyes
+     fill(255);
+     let small_circle1_size = pupil_size * 0.5;
+     let small_circle2_size = pupil_size * 0.3;
+     let small_circle1_offset = pupil_size * 0.2;
+     let small_circle2_offset = pupil_size * 0.1;
+     ellipse(-3 - pupil_offset + small_circle1_offset, -2 - small_circle1_offset, small_circle1_size, small_circle1_size);
+     ellipse(-3 - pupil_offset + small_circle2_offset, -2 + small_circle2_offset, small_circle2_size, small_circle2_size);
+     ellipse(3 + pupil_offset - small_circle1_offset, -2 - small_circle1_offset, small_circle1_size, small_circle1_size);
+     ellipse(3 + pupil_offset - small_circle2_offset, -2 + small_circle2_offset, small_circle2_size, small_circle2_size);
+     
+     // draw mouth
+     fill(255, 100, 100);
+     let mouth_height = map(this.mouth_value, 0, 100, 0, 2); // map the mouth value to a range of 0-2
+     rect(-1, 2, 2, mouth_height, 1); // draw a rectangle for the mouth
+    // console.log()
+    // // head
+    // ellipseMode(CENTER);
+    // stroke(stroke_color);
+    // fill(this.mainColour);
+    // ellipse(segment_average(positions.chin)[0], 0, 3, 4);
+    // noStroke();
+
+
+    // // mouth
+    // fill(this.detailColour);
+    // ellipse(segment_average(positions.bottom_lip)[0], segment_average(positions.bottom_lip)[1], 1.36, 0.25 * this.mouth_size);
+
+    // // eyebrows
+    // fill( this.eyebrowColour);
+    // stroke( this.eyebrowColour);
+    // strokeWeight(0.08);
+    // this.draw_segment(positions.left_eyebrow);
+    // this.draw_segment(positions.right_eyebrow);
+
+    // // draw the chin segment using points
+    // fill(this.chinColour);
+    // stroke(this.chinColour);
+    // this.draw_segment(positions.chin);
+
+    // fill(100, 0, 100);
+    // stroke(100, 0, 100);
+    // this.draw_segment(positions.nose_bridge);
+    // this.draw_segment(positions.nose_tip);
+
+    // strokeWeight(0.03);
+
+    // fill(this.lipColour);
+    // stroke(this.lipColour);
+    // this.draw_segment(positions.top_lip);
+    // this.draw_segment(positions.bottom_lip);
+
+    // let left_eye_pos = segment_average(positions.left_eye);
+    // let right_eye_pos = segment_average(positions.right_eye);
+
+    // // eyes
+    // noStroke();
+    // let curEyeShift = 0.04 * this.eye_shift;
+    // if(this.num_eyes == 2) {
+    //   fill(this.detailColour);
+    //   ellipse(left_eye_pos[0], left_eye_pos[1], 0.5, 0.33);
+    //   ellipse(right_eye_pos[0], right_eye_pos[1], 0.5, 0.33);
+
+    //   // fill(this.mainColour);
+    //   // ellipse(left_eye_pos[0] + curEyeShift, left_eye_pos[1], 0.18);
+    //   // ellipse(right_eye_pos[0] + curEyeShift, right_eye_pos[1], 0.18);
+    // }
+    // else {
+    //   let eyePosX = (left_eye_pos[0] + right_eye_pos[0]) / 2;
+    //   let eyePosY = (left_eye_pos[1] + right_eye_pos[1]) / 2;
+
+    //   fill(this.detailColour);
+    //   ellipse(eyePosX, eyePosY, 0.45, 0.27);
+
+    //   fill(this.mainColour);
+    //   ellipse(eyePosX - 0.1 + curEyeShift, eyePosY, 0.18);
+    // }
    // fill(0)
    //ellipse(0,0, 0.5,0.5) center point
    //rect(-2,-2,4.5,4) sizing debug 
