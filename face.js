337bac1a60241462cfbb4bba3f22b03afec06e92
LouisJefferson607 <100631217+LouisJefferson607@users.noreply.github.com>
2023-06-06 17:22:59 +1200

    Mapping
    Have made the mapping sliders work from my previous file and about to start on making it reactive to the face

--- face.js
+++ face.js
@@ -7,7 +7,7 @@
 var DEBUG_MODE = true;
 
 // this can be used to set the number of sliders to show
-var NUM_SLIDERS = 3;
+var NUM_SLIDERS = 4;
 
 // other variables can be in here too
 // here's some examples for colors used
@@ -30,24 +30,12 @@ function segment_average(segment) {
 
 // This where you define your own face object
 function Face(eye_value, mouth_value, chocolate_value, NS, chocolate_size) {
-  // these are state variables for a face
-  // (your variables should be different!)
-  this.detailColour = [204, 136, 17];
-  this.mainColour = [51, 119, 153];
-  this.num_eyes = 2;    // can be either 1 (cyclops) or 2 (two eyes)
-  this.eye_shift = -1;   // range is -10 to 10
-  this.mouth_size = 1;  // range is 0.5 to 8
-
-  this.chinColour = [153, 153, 51]
-  this.lipColour = [136, 68, 68]
-  this.eyebrowColour = [119, 85, 17]
-
-  this.eye_value = int(random(3, 8));
-  this.mouth_value = random(0, 150);
-  this.chocolate_value = int(random(0, 14));
-  this.height_cook = int(random(canvasHeight / 2 - 140, canvasHeight / 2 + 140));
-  this.width_cook = int(random(canvasWidth /  2 - 250, canvasWidth / 2 + 250));
-  this.chocolate_size = int(random(1, 2));
+  this.eye_value = 5;
+  this.mouth_value = 75
+  this.chocolate_value = 7
+  this.height_cook = 20
+  this.width_cook = 20
+  this.chocolate_size = 1
 
   /*
    * Draw the face with position lists that include:
@@ -55,6 +43,12 @@ function Face(eye_value, mouth_value, chocolate_value, NS, chocolate_size) {
    *    bottom_lip, top_lip, nose_tip, nose_bridge, 
    */  
   this.draw = function(positions) {
+
+
+  push()
+  scale(0.3)
+
+
     let cookie_colors = [
       {color: color(132, 86, 60), weight: 5},
       {color: color(217, 190, 145), weight: 3},
@@ -169,6 +163,8 @@ function Face(eye_value, mouth_value, chocolate_value, NS, chocolate_size) {
      fill(255, 100, 100);
      let mouth_height = map(this.mouth_value, 0, 100, 0, 2); // map the mouth value to a range of 0-2
      rect(-1, 2, 2, mouth_height, 1); // draw a rectangle for the mouth
+
+     pop()
     // console.log()
     // // head
     // ellipseMode(CENTER);
@@ -258,17 +254,26 @@ function Face(eye_value, mouth_value, chocolate_value, NS, chocolate_size) {
 
   /* set internal properties based on list numbers 0-100 */
   this.setProperties = function(settings) {
-    this.num_eyes = int(map(settings[0], 0, 100, 1, 2));
-    this.eye_shift = map(settings[1], 0, 100, -2, 2);
-    this.mouth_size = map(settings[2], 0, 100, 0.5, 8);
+    this.eye_value = int(map(settings[0], 0, 100, 1, 10));
+    this.mouth_value = map(settings[1], 0, 100, 0, 150);
+    this.chocolate_value = map(settings[2], 0, 100, 0, 10);
+    this.chocolate_size = map(settings[3], 0, 100, 0, 2);
   }
 
   /* get internal properties as list of numbers 0-100 */
   this.getProperties = function() {
     let settings = new Array(3);
-    settings[0] = map(this.num_eyes, 1, 2, 0, 100);
-    settings[1] = map(this.eye_shift, -2, 2, 0, 100);
-    settings[2] = map(this.mouth_size, 0.5, 8, 0, 100);
+    settings[0] = map(this.eye_value, 1, 10, 0, 100);
+    settings[1] = map(this.mouth_value, 0, 150, 0, 100);
+    settings[2] = map(this.chocolate_value, 0, 10, 0, 100);
+    settings[3] = map(this.chocolate_size, 0, 2, 0, 100);
     return settings;
   }
 }
+
+// this.eye_value = 5;
+//   this.mouth_value = 75
+//   this.chocolate_value = 7
+//   this.height_cook = 20
+//   this.width_cook = 20
+//   this.chocolate_size = 1
